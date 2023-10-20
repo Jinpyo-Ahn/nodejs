e1af73f99b2fae17b5dc4188a1351409694d9ac3
Jinpyo-Ahn <wlsvy3312@o365.hanbat.ac.kr>
2023-10-20 19:54:47 +0900

    2. 알아둬야 할 자바스크립트

---
+++ Chapter02/ex2-6.js
@@ -0,0 +1,16 @@
+// const candyMachine = {
+//     status: {
+//         name: "node",
+//         count: 5,
+//     },
+//     getCandy() {
+//         this.status.count--;
+//         return this.status.count;
+//     },
+// };
+//
+// const {getCandy, status: {count}} = candyMachine;
+
+const array = ["nodejs", {}, 10, true];
+const [node, obj, , bool] = array;
+
